3a701749d6a3492caff547ecb5e95d7e218a5564
felevi <86256508+felevi@users.noreply.github.com>
2021-06-21 17:44:30 -0300

    Update log_ssh.sh

--- log_ssh.sh
+++ log_ssh.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-sshpass -p "felipe@2020" ssh felipe@localhost ps aux > log.txt; exit
+sshpass -p "attus@2020" ssh attus@localhost ps aux > log.txt;
+
+while getopts "ch" OPT; do
+	case "$OPT" in
+	c) crontab -l | { cat; echo "15 * * * * log_ssh.sh"; } | crontab -;;
+	h) echo "Caso seja a primeira execução, usar -c para adicionar regra no crontab."
+	esac
+done
